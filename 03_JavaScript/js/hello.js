9a398415cbaa1d8ce7f2651e18206fdcba47c90a
Ingress <m13294144031@163.com>
2024-04-25 14:22:48 +0800

    更新

--- 03_JavaScript/js/hello.js
+++ 03_JavaScript/js/hello.js
@@ -1,2 +1,11 @@
-var age = 30;
-console.log(age);
+// var age = 30;
+// console.log(age);
+
+
+var obj = JSON.parse($response.body);
+if (-1 != $request.url.indexOf("bicycle/v1/vehicle/dynamicInfo")) {
+    if ( obj.errcode == 0 && obj.data){
+        obj.data.vehicleBlockStatus = 1;
+        $done({body: JSON.stringify(obj)});
+    }
+} else $done({});
