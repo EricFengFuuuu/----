91d3eb24c1ed954c13e23b55985f4efa99b10d73
Ingress <m13294144031@163.com>
2024-04-25 14:49:03 +0800

    更新

--- 03_JavaScript/js/hello.js
+++ 03_JavaScript/js/hello.js
@@ -3,10 +3,23 @@
 
 //v1.0
 
+console.log($request.url);
+console.log($request.body);
+
 var obj = JSON.parse($response.body);
 if (-1 != $request.url.indexOf("bicycle/v1/vehicle/dynamicInfo")) {
     if ( obj.errcode == 0 && obj.data){
         obj.data.vehicleBlockStatus = 1;
         $done({body: JSON.stringify(obj)});
     }
-} else $done({});
+}
+else if (-1 != $request.url.indexOf("bicycle/api/function/getFuncList")) {
+    if ( obj.errcode == 0 && obj.data){
+        obj.data.showBackupBatterySoc.support = 1;
+        obj.data.showBackupBatterySoc.support = 1;
+        $done({body: JSON.stringify(obj)});
+    }
+}
+else {
+    $done({});
+} ;
