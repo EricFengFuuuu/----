3053cb9d9ab72b6d6e453b560337d84d25766aad
Ingress <m13294144031@163.com>
2024-06-28 10:10:15 +0800

    更新

--- 03_JavaScript/js/hello.js
+++ 03_JavaScript/js/hello.js
@@ -1,30 +1,3 @@
-// var age = 30;
-// console.log(age);
+var age = 30;
+console.log(age);
 
-//v1.0
-
-var obj = JSON.parse($response.body);
-if (-1 != $request.url.indexOf("bicycle/v1/vehicle/dynamicInfo")) {
-    if ( obj.errcode == 0 && obj.data){
-        // obj.data.vehicleBlockStatus = 1;
-        $done({body: JSON.stringify(obj)});
-    }
-}
-else if (-1 != $request.url.indexOf("bicycle/api/function/getFuncList")) {
-    if ( obj.errcode == 0 && obj.data){
-        // obj.data.showBackupBatterySoc.support = 1;
-        // obj.data.resetVehicle.support = 1;
-        // obj.data.vehicleShared.support = 1;
-        $done({body: JSON.stringify(obj)});
-    }
-}
-else if (-1 != $request.url.indexOf("bicycle/api/trafficPackage/getTrafficOverviewExpiredDate")) {
-    if ( obj.errcode == 0 && obj.data){
-        obj.data.expirationDate = 1743091199000;
-        obj.data.status = 1;
-        $done({body: JSON.stringify(obj)});
-    }
-}
-else {
-    $done({});
-} ;
---
+++ 03_JavaScript/js/mengma.js
@@ -0,0 +1,28 @@
+//v1.0
+//猛犸电动App智能服务破解
+
+var obj = JSON.parse($response.body);
+if (-1 != $request.url.indexOf("bicycle/v1/vehicle/dynamicInfo")) {
+    if ( obj.errcode == 0 && obj.data){
+        // obj.data.vehicleBlockStatus = 1;
+        $done({body: JSON.stringify(obj)});
+    }
+}
+else if (-1 != $request.url.indexOf("bicycle/api/function/getFuncList")) {
+    if ( obj.errcode == 0 && obj.data){
+        // obj.data.showBackupBatterySoc.support = 1;
+        // obj.data.resetVehicle.support = 1;
+        // obj.data.vehicleShared.support = 1;
+        $done({body: JSON.stringify(obj)});
+    }
+}
+else if (-1 != $request.url.indexOf("bicycle/api/trafficPackage/getTrafficOverviewExpiredDate")) {
+    if ( obj.errcode == 0 && obj.data){
+        obj.data.expirationDate = 1743091199000;
+        obj.data.status = 1;
+        $done({body: JSON.stringify(obj)});
+    }
+}
+else {
+    $done({});
+} ;
