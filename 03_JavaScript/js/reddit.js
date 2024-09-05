69a203537ad9f2357d05e9fbe5e0b101ec91b671
EricFeng <1432073362@qq.com>
2024-09-05 21:33:49 +0800

    修改脚本

--- 03_JavaScript/js/reddit.js
+++ 03_JavaScript/js/reddit.js
@@ -5,19 +5,22 @@ var requestUrl = $request.url;
 var statusCode = $response.statusCode;
 var obj = JSON.parse($response.body);
 
+console.log("statusCode："+ statusCode);
+console.log("data:" + obj.data);
+
  if (-1 != requestUrl.indexOf("https://gql-fed.reddit.com/")) {
     if ( statusCode == 200 && obj.data){
 
-        console.log(obj.data.homeV3);
+        
 
-        if(obj.data.homeV3.elements.edges.length > 0){
-            obj.data.homeV3.elements.edges.forEach(function (item) {
-                if (item.node.adPayload != null) {
-                    item.node.adPayload = null;
+        // if(obj.data.homeV3.elements.edges.length > 0){
+        //     obj.data.homeV3.elements.edges.forEach(function (item) {
+        //         if (item.node.adPayload != null) {
+        //             item.node.adPayload = null;
                     
-                }
-            });
-        }
+        //         }
+        //     });
+        // }
 
         $done({body: JSON.stringify(obj)});
     }
