46d5adb2eaccab11a46988e9b17e4433baea7688
Ingress <m13294144031@163.com>
2024-10-17 11:47:40 +0800

    更新

---
+++ 10_Webpack/projectDemo01/src/common.js
@@ -0,0 +1,4 @@
+exports.domWrite = function (str) {
+    document.write("<p>" + str + "</p>");
+    console.log(str)
+}
---
+++ 10_Webpack/projectDemo01/src/main.js
@@ -0,0 +1,5 @@
+const common = require("./common.js");
+const utils = require("./utils.js");
+
+
+common.domWrite("计算结果:"+ utils.add(1, 2));
